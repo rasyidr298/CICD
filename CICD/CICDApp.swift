48853af55527278192ccb070d7a443055e0e0924
Rasyid <rasydridla298@gmail.com>
2021-10-08 11:00:13 +0700

    Initial Commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by Rasyid Ridla on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
